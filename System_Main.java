9b608f294f0f329f457dab984bfe985a795756d6
sugatmoon <sugatmoon17@gmail.com>
2025-12-08 19:21:12 +0530

    for every CRUD opration I have been create a switch case accordingly

--- System_Main.java
+++ System_Main.java
@@ -1,3 +1,43 @@
+import java.util.Scanner;
+
 public class System_Main {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        
+        System.out.println("***Employee Mangement System***");
+        
+        do{
+
+            System.out.printf(" 1) VIEW\n 2) ADD\n 3) UPDATE\n 4) DELETE\n 5) EXIT\n\n->");
+            int option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    // VIEW();
+                    break;
+                case 2:
+                    // ADD();
+                    break;
+                case 3:
+                    // UPDATE();
+                    break;
+                case 4:
+                    // DELETE();
+                    break;
+                case 5:
+                    // EXIT();
+                    flag = false;
+                    System.out.println("EXIT Sucessfully !!!!!!!!");
+                    break;
+            
+                default:
+                    System.out.println("Please Inter Valid Input !!");
+            }
+        
+        }while(flag);
+        
+        sc.close();
+    }
 }
